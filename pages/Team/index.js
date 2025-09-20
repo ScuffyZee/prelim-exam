6f504bc0d6ae0617e6174ccba128b41bf6f67172
Zidane Lineses <zdnjsf.temp@gmail.com>
2025-09-20 10:35:20 +0800

    commit4

--- pages/Team/index.js
+++ pages/Team/index.js
@@ -1,8 +1,11 @@
+import React from 'react'
+const Team = () => {
+  return (
 <section class="text-gray-400 bg-gray-900 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="text-2xl font-medium title-font mb-4 text-white tracking-widest">OUR TEAM</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven&apos;t heard of them.</p>
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="p-4 lg:w-1/2">
@@ -116,3 +119,7 @@
     </div>
   </div>
 </section>
+  )
+}
+
+export default Team
